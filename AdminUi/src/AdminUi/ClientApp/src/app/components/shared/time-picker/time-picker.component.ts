b0dc7bc225488b2ea925c158113a85d89b8676ad
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-09-27 16:47:05 +0100

    feat: make timepicker use number inputs instead

--- AdminUi/src/AdminUi/ClientApp/src/app/components/shared/time-picker/time-picker.component.ts
+++ AdminUi/src/AdminUi/ClientApp/src/app/components/shared/time-picker/time-picker.component.ts
@@ -7,10 +7,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class TimePickerComponent implements OnInit {
     public hour = 0;
-    public hours = [...Array(24).keys()]
-
     public minute = 0;
-    public minutes = [...Array(60).keys()]
     
     public ngOnInit(): void {
         throw new Error("Method not implemented.");
